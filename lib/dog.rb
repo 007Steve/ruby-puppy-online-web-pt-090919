7e5079ecf05fec48f31392103b4791f2957f9e9d
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-10-18 01:02:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+class Dog
+
+  attr_accessor :name,:save
+  
+
+  @@all = []
+
+  def initialize(name)
+    
+    @name = name
+    @@all << self
+    @save
+  end
+
+  def self.all
+@@all
+    end
+  
+  def self.print_all
+    self.all.each do |dog_object| 
+     puts dog_object.name
+  end
+end
+
+  def self.clear_all
+    @@all.clear
+  end
+  def save
+    @@all<< self
+  end
+
+end
